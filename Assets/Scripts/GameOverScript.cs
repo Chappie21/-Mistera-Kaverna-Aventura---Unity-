e3d030a383ca5d5ddb6117124da987292fe820ef
JoseAIG <inciartegj@gmail.com>
2021-04-20 22:47:16 -0400

    GameOver y Pausa mejorada

---
+++ Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour
+{
+    public GameObject vistaGameOver;
+    public static bool jugadorMuerto;
+
+    void Start(){
+        vistaGameOver.SetActive(false);
+    }
+
+    void Update(){
+        if(PlayerController.vidaActual<=0){
+            vistaGameOver.SetActive(true);
+            // Time.timeScale=0f;
+        }
+    }
+
+    public void Continuar(){
+        // Time.timeScale=1f;
+        SceneManager.LoadScene("MenuScene");
+    }
+}
